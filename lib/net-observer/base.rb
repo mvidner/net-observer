201ad81ccc0635c7b05561fbdd3d78832ce5083d
Josef Reidinger <jreidinger@suse.cz>
2010-12-13 15:59:13 +0100

    Add logger class and split module name from class

--- lib/net-observer/base.rb
+++ lib/net-observer/base.rb
@@ -1,14 +1,16 @@
-class NetObserver::Base
-  include Observable
-	include Singleton
+module NetObserver
+	class Base
+  	include Observable
+		include Singleton
 
-	def request_data(request,body)
-		changed
-		notify_observers(:request,request,body) 
-	end
+		def request_data(request,body)
+			changed
+			notify_observers(:request,request,body) 
+		end
 
-	def request_data(request,body)
-		changed
-		notify_observers(:response,request,body) 
+		def request_data(request,body)
+			changed
+			notify_observers(:response,request,body) 
+		end
 	end
 end
---
+++ lib/net-observer/net_logger.rb
@@ -0,0 +1,12 @@
+module NetObserver
+	class NetLogger
+		def initialize(logger)
+			@logger.logger
+			Base.instance.add_observer(self)
+		end
+
+		def update(type, request, body)
+			@logger.info "get #{type}: #{request.inspect} with body #{body}"
+		end
+	end
+end
