bd34347b8fd4819cf2ab6228bce4d5b219343282
Josef Reidinger <jreidinger@suse.cz>
2010-12-14 13:22:34 +0100

    add more documentation

--- lib/core-ext/net/http.rb
+++ lib/core-ext/net/http.rb
@@ -5,14 +5,10 @@ module Net
   class HTTP
     alias_method :orig_request, :request
 
+		# hook into net code to get request and response for it
     def request(req, body = nil, &block)  # :yield: +response+
-      begin
-        NetObserver::Base.instance.request_data(self, req, body)
-        res = orig_request req, body, &block
-      rescue => e
-        puts "something went wrong: #{e.message}"
-        raise e
-      end
+      NetObserver::Base.instance.request_data(self, req, body)
+      res = orig_request req, body, &block
       NetObserver::Base.instance.response_data(self, res)
       res
     end
--- lib/net_observer/logger.rb
+++ lib/net_observer/logger.rb
@@ -1,12 +1,22 @@
 require "net_observer/base"
 
 module NetObserver
+# == Logger class
+# It can be used to pass network communication into logger.
+# It report requests and success response as info and failed one as 
+# warn message.
+# @example Set simple logging to console
+#   NetObserver::Logger.new Logger.new STDOUT
 	class Logger
+# register logger to receiving infos about network communication
+# @param(Logger) logger which recieve informations about communication
 		def initialize(logger)
 			@logger = logger
 			Base.instance.add_observer(self)
 		end
 
+# methods to receive information about connection. Needed to allow itself to register agains Base
+# @see NetObserver::Base for parameters details
 		def update(type, request, body, connection)
 			case type
 			when :response
