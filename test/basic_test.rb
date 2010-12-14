a6a355c5d861d423669a5c25b833d3895d75de58
Flavio Castelli <fcastelli@novell.com>
2010-12-14 13:20:45 +0100

    Improve our unit tests

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'test_helper'
 require "logger"
-
+require 'mocha'
 require "net_observer"
 
 class BasicTest < Test::Unit::TestCase
@@ -10,12 +10,20 @@ class BasicTest < Test::Unit::TestCase
   end
 
   def test_it_by_watching
+    url = "http://google.com/search?q=wikipedia"
+    uri = URI.parse url
     log = Logger.new(STDOUT)
     net_logger = NetObserver::NetLogger.new log
 
-    register_fake_response :get, "http://google.com/search?q=wikipedia",
-                           "wikipedia"
+    reply = File.read(File.join( File.dirname(__FILE__), "mocked_responses", "wikipedia"))
+    html_body = reply[reply.index("<html>")..reply.size]
+    log.expects(:info).with(regexp_matches(/get request for url http?:\/\/#{uri.host}:#{uri.port}#{uri.path}\?#{uri.query} with method: GET with body /)).once
+    
+    # make sure response contents are printed too
+    log.expects(:warn).with(includes(html_body)).once
+    
+    register_fake_response :get, url , "wikipedia"
 
-    Net::HTTP.get URI.parse("http://google.com/search?q=wikipedia")
+    Net::HTTP.get URI.parse(url)
   end
 end
