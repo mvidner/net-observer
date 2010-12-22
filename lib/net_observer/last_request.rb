9a6c93633b9cf0ccef9abcb80a83b5f735bd1f81
Josef Reidinger <jreidinger@suse.cz>
2010-12-22 11:30:38 +0100

    update documentaion

--- lib/net_observer/last_request.rb
+++ lib/net_observer/last_request.rb
@@ -2,26 +2,35 @@ require "net_observer/base"
 require "singleton"
 
 module NetObserver
-# == Logger class
-# It can be used to pass network communication into logger.
-# It report requests and success response as info and failed one as 
-# warn message.
-# @example Set simple logging to console
-#   NetObserver::Logger.new Logger.new STDOUT
+# == Last Request class
+# Its main purpose is to remember last request send via network.
+# @example Enable last request and print it when Connection Error appear
+#   NetObserver::LastRequest.instance.enable
+#   begin
+#     ..
+#   rescue ActiveResource::ConnectionError => e
+#     last_request = NetObserver::LastRequest.instance.last_request
+#     site = "#{last_request[:connection].use_ssl? ? "https" : "http"}://"+last_request[:connection].address
+#     log.error "Job failed due to communication problem with #{site}."
+#     log.error "Request: path: #{last_request[:request].path} with body #{last_request[:body]}."
+#     log.error "Response: #{e.inspect} with body #{e.response.body}."
+#   end
 	class LastRequest
 		include Singleton
+#last request sent via network
 		attr_reader :last_request
-# register logger to receiving infos about network communication
-# @param(Logger) logger which recieve informations about communication
+# Enable storing last request
 		def enable
 			@last_request = nil
 			Base.instance.add_observer(self)
 		end
 
+# Disable storing requests
 		def disable
 			@last_request = nil
 			Base.instance.delete_observer(self)
 		end
+
 # methods to receive information about connection. Needed to allow itself to register agains Base
 # @see NetObserver::Base for parameters details
 		def update(type, request, body, connection)
